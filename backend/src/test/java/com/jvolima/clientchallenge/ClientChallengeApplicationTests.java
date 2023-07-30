3b9a48d43ed3028f9a9b2b3199293f3decd3fea8
João Vitor <jvolima2004@gmail.com>
2023-07-30 14:42:59 -0300

    chore: setup project

---
+++ backend/src/test/java/com/jvolima/clientchallenge/ClientChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvolima.clientchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
