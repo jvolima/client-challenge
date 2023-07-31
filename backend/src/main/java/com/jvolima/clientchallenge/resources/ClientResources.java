10d71dba6e07352ff6e80d5460f8f8ac476705ab
João Vitor <jvolima2004@gmail.com>
2023-07-30 21:51:13 -0300

    feat: clients seed

--- backend/src/main/java/com/jvolima/clientchallenge/resources/ClientResources.java
+++ backend/src/main/java/com/jvolima/clientchallenge/resources/ClientResources.java
@@ -24,7 +24,7 @@ public class ClientResources {
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "id") String orderBy
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
 			) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		Page<ClientDTO> list = service.findAllPaged(pageRequest);
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,1 +1,11 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joao Vitor de Oliveira Lima', '98754236574', 5400.0, TIMESTAMP WITH TIME ZONE '2004-10-11T15:40:02Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joao Vitor', '98754236574', 8400.0, TIMESTAMP WITH TIME ZONE '2004-10-11T15:40:02Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Lucas Gabriel', '65421036574', 5500.0, TIMESTAMP WITH TIME ZONE '2004-08-07T18:10:05Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Giovani Ocan', '12569458741', 7200.0, TIMESTAMP WITH TIME ZONE '2004-05-20T21:12:21Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Otavio Marques', '65402485146', 5400.0, TIMESTAMP WITH TIME ZONE '2004-07-21T08:52:10Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Adrian kaiky', '87456520145', 3400.0, TIMESTAMP WITH TIME ZONE '2004-04-15T17:23:54Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Victor Henrique', '58798774521', 5100.0, TIMESTAMP WITH TIME ZONE '2004-01-18T04:35:47Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Felipe Kenjy', '80145468740', 6800.0, TIMESTAMP WITH TIME ZONE '2005-05-01T20:17:14Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Eduardo Lopes', '34569554120', 2950.0, TIMESTAMP WITH TIME ZONE '2006-12-04T11:00:35Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Eduardo Souza', '12405468794', 5300.0, TIMESTAMP WITH TIME ZONE '2003-10-24T20:15:24Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joao Lucas', '02134567894', 7400.0, TIMESTAMP WITH TIME ZONE '2003-01-03T11:10:52Z', 1);
+
