9f4e4f0f83b94804d554272d6ec6529b79a3f40c
João Vitor <jvolima2004@gmail.com>
2023-07-31 10:31:13 -0300

    feat: update clients

--- backend/src/main/java/com/jvolima/clientchallenge/services/ClientService.java
+++ backend/src/main/java/com/jvolima/clientchallenge/services/ClientService.java
@@ -2,6 +2,8 @@ package com.jvolima.clientchallenge.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -36,13 +38,30 @@ public class ClientService {
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		
+		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found.");
+		}
+	}
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
-		entity = repository.save(entity);
-		
-		return new ClientDTO(entity);
 	}
 }
