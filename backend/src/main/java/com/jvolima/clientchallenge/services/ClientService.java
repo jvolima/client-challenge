1798ab62bc8a122b88b20d3c17fc63951738e985
João Vitor <jvolima2004@gmail.com>
2023-07-31 10:51:34 -0300

    feat: delete clients

--- backend/src/main/java/com/jvolima/clientchallenge/services/ClientService.java
+++ backend/src/main/java/com/jvolima/clientchallenge/services/ClientService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -53,6 +54,14 @@ public class ClientService {
 			
 			return new ClientDTO(entity);
 		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found.");
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException("Id " + id + " not found.");
 		}
 	}
